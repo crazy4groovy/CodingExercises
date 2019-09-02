4bab88efb00e1472ffeba0689fe413f1fa634b5d
crazy4groovy <crazy4groovy@users.noreply.github.com>
2019-09-02 00:00:39 -0400

    tab -> space

--- completed_exercises/levelwidth/index.js
+++ completed_exercises/levelwidth/index.js
@@ -34,16 +34,16 @@ module.exports = levelWidth;
 
 /*
 function process(node, i, m) {
-	if (node.children.length === 0) return
-	m[i + 1] = m[i + 1] || 0
+  if (node.children.length === 0) return
+  m[i + 1] = m[i + 1] || 0
   m[i + 1] += node.children.length
-	node.children.forEach(c => process(c, i + 1, m))
+  node.children.forEach(c => process(c, i + 1, m))
 }
 
 function levelWidth(root) {
   const m = [1]
   process(root, 0, m)
-	console.log(m)
+  console.log(m)
 }
 
 module.exports = levelWidth;
